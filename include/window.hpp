3a0dff6f7626b6c3412e19166fd7e72c234254f2
Ryan <ruiyang.xu12@gmail.com>
2025-06-24 19:50:13 -0700

    add swapchain

--- include/window.hpp
+++ include/window.hpp
@@ -1,13 +1,19 @@
 #pragma once
+
 #include <GLFW/glfw3.h>
 
 class VulkanWindow {
-    public:
-        VulkanWindow();
-        ~VulkanWindow();
-        VulkanWindow(const VulkanWindow&) = delete;
-        VulkanWindow operator = (const VulkanWindow&) = delete;
-        void run();
-    private:
-        GLFWwindow* window_;
+public:
+    VulkanWindow();
+    ~VulkanWindow();
+    VulkanWindow(const VulkanWindow&) = delete;
+    VulkanWindow& operator=(const VulkanWindow&) = delete;
+
+    void run();
+
+    // Getter for GLFW window
+    GLFWwindow* getWindow() const { return window_; }
+
+private:
+    GLFWwindow* window_;
 };
